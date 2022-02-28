bff2309bea53915ae67aae40d9da185eb1538cd7
Gabriel Molinski <gabriel.molinski@gmail.com>
2022-02-28 16:58:51 +0100

    Added client file

---
+++ client.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+def addToDb():
+    pass
+
+
+root = tk.Tk()
+root.title("Client")
+
+canvas = tk.Canvas(root, height=700, width=700, bg="#263D42")
+canvas.pack()
+
+root.mainloop()
